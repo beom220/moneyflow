a9ff8a8fd5d5b7f4c4e023ef5ff1ec0e4d8a4b7f
beom220 <beom220@gmail.com>
2022-07-17 18:22:19 +0900

    [ feat ] recoil logger

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './assets/css/reset.css';
 import './assets/css/app.css';
+import {RecoilRoot} from "recoil";
+import {DebugObserver} from "./app/debug";
+
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RecoilRoot>
+            <DebugObserver/>
+            <App/>
+        </RecoilRoot>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
