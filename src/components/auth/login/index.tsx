d724a1cd26da9597d07d5d7c4d97803a083c9817
beom220 <beom220@gmail.com>
2022-07-23 02:05:29 +0900

    googleLogin 동작구현 테스트 성공 #8

---
+++ src/components/auth/login/index.tsx
@@ -0,0 +1,1 @@
+export {default as GoogleLoginForm} from './google'
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,7 +1,9 @@
+import {GoogleLoginForm} from "../../components/auth/login";
 export default function Login(){
     return (
         <>
             <p>로그인</p>
+            <GoogleLoginForm/>
         </>
     )
 }
